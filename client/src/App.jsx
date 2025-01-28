e2021f8c6e89ad37f8d5a97b24109ac2728e5e09
Titan007 <sanskargambhir93@gmail.com>
2025-01-28 15:40:05 +0530

    Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import ExamPage from "./pages/ExamPage";
 import Login from "./pages/login";
 import SignUp from "./pages/SignUp";
-//import AdminDashboard from "./pages/Admin Pages/AdminDashboard";
+import AdminDashboard from "./pages/Admin Pages/AdminDashboard";
 import "./App.css";
 
 function App() {
@@ -14,19 +14,29 @@ function App() {
 
   return (
     <Router>
-      <div>
-        <Navbar />
-        <main className="flex-grow">
-          <Routes> 
-            <Route path="/" element={<HomePage />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/exam/:examId" element={<ExamPage />} />  
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <AppContent />
     </Router>
+  );
+}
+
+function AppContent() {
+  const location = useLocation(); // Get the current route path
+  const hideNavAndFooter = location.pathname.startsWith("/Admin"); // Check if the current path is Admin-related
+
+  return (
+    <div>
+      {!hideNavAndFooter && <Navbar />}
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/exam/:examId" element={<ExamPage />} />
+          <Route path="/Admin" element={<AdminDashboard />} />
+        </Routes>
+      </main>
+      {!hideNavAndFooter && <Footer />}
+    </div>
   );
 }
 
